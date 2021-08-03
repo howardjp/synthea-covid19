8294a6b37533a1df4fbfe7d76729d3ac9ad47e0e
James P. Howard, II <james.howard@jhuapl.edu>
2021-08-03 09:47:32 -0400

    RMd the CSV files

--- munge/06-abbrevs.R
+++ munge/06-abbrevs.R
@@ -3,10 +3,12 @@ covid19.dt <- covid19.dt.bk
 info(logger, "Starting assembly of abbreviated dataset")
 patient_list <- sort(unique(covid19.dt[, PATIENT]))
 patient_list <- data.table(PATIENT = sort(sample(patient_list, 30)))
-best12 <- names(sort(colSums(sapply(covid19.dt, FUN = is.na)))[1:12])
+best12 <- names(sort(colSums(sapply(covid19.dt, FUN = is.na)))[1:16])
 best12 <- c("DINDEX", best12)
 covid19.dt <- covid19.dt[, ..best12]
 covid19.dt <- merge(patient_list, covid19.dt, by = "PATIENT", all.x = TRUE)
 filename <- "output/abbrev.json"
 
 make_json(covid19.dt, patient_list[, PATIENT], filename)
+
+exit()
